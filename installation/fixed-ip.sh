f55b6efd8cd50f0731c5db0abc47e9d28d536553
C. Molendijk <cmolendijk@users.noreply.github.com>
2024-02-14 22:25:18 +0100

    Improve setting an fixed IP

--- installation/fixed-ip.sh
+++ installation/fixed-ip.sh
@@ -6,16 +6,33 @@ echo "Setting up fixed IP address"
 echo "Please enter the IP address:"
 read IP_ADDRESS
 
+# Ask for the gateway address
+echo "Please enter the default gateway:"
+read GATEWAY_ADDRESS
+
+# Ask for the DNS server
+echo "Please enter the DNS server:"
+read DNS_SERVER
+
 # Get the network interfaces
-echo "Network interfaces:"
+echo "Available network interfaces:"
 nmcli con show
 
 # Ask for the network interface name
-echo "Please enter the network interface name (e.g., eth0):"
+echo "Please enter the network interface name for the fixed IP (e.g., eth0):"
 read INTERFACE_NAME
 
 # Set the static IP address
-sudo nmcli con mod $INTERFACE_NAME ipv4.addresses $IP_ADDRESS/24 ipv4.method manual
+sudo nmcli con mod $INTERFACE_NAME ipv4.addresses $IP_ADDRESS/24
+
+# Set the gateway
+sudo nmcli con mod $INTERFACE_NAME ipv4.gateway $GATEWAY_ADDRESS
+
+# Set the DNS server
+sudo nmcli con mod $INTERFACE_NAME ipv4.dns $DNS_SERVER
+
+# Set the connection to be automatically connected
+sudo nmcli con mod $INTERFACE_NAME ipv4.method manual
 
 # Bring the interface down and up to apply the changes
 sudo nmcli con down $INTERFACE_NAME
