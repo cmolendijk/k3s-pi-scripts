ee466abdc94678a99771593e9e663ca21735b95d
C. Molendijk <cmolendijk@users.noreply.github.com>
2024-02-14 20:03:08 +0100

    Make cloned folder available for all users

--- getting-started.sh
+++ getting-started.sh
@@ -4,5 +4,6 @@ apt-get update && apt-get upgrade -y
 apt-get install git -y
 mkdir -p /root/repos
 git clone https://github.com/cmolendijk/k3s-pi-scripts.git /root/repos/k3s-pi-scripts
+chmod 777 /root/repos/k3s-pi-scripts
 
 echo "Done getting ready to install k3s on a Raspberry Pi."
