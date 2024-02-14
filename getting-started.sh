d07d3a7935a618cc4780a1d79f2efa8cd70a761b
C. Molendijk <cmolendijk@users.noreply.github.com>
2024-02-14 19:44:38 +0100

    Make te start use correct folder to get repos in

--- getting-started.sh
+++ getting-started.sh
@@ -2,8 +2,7 @@ echo "Getting ready to install k3s on a Raspberry Pi"
 
 apt-get update && apt-get upgrade -y
 apt-get install git -y
-mkdir $HOME/repos
-cd $HOME/repos
-git clone https://github.com/cmolendijk/k3s-pi-scripts.git
+mkdir -p $HOME/repos
+git clone https://github.com/cmolendijk/k3s-pi-scripts.git $HOME/repos/k3s-pi-scripts
 
 echo "Done getting ready to install k3s on a Raspberry Pi."
