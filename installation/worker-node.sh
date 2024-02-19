6633b3a8d533f07c3b787bdac9055483bfad86d0
C. Molendijk <cmolendijk@users.noreply.github.com>
2024-02-19 21:25:23 +0100

    Update worker-node.sh

--- installation/worker-node.sh
+++ installation/worker-node.sh
@@ -7,8 +7,4 @@ bash $HOME/repos/k3s-pi-scripts/installation/upgrade.sh
 bash $HOME/repos/k3s-pi-scripts/installation/k3s-worker-node-install.sh
 bash $HOME/repos/k3s-pi-scripts/installation/helm-install.sh
 
-echo "Copy k3s configuration file to Kubernets default directory"
-mkdir $HOME/.kube
-sudo cp /etc/rancher/k3s/k3s.yaml $HOME/.kube/.
-
 echo "Control node configuration set up."
