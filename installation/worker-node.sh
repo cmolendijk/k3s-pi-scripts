dcfb77b1660d51fc125dc19a3c400a8c6d2c49c5
C. Molendijk <cmolendijk@users.noreply.github.com>
2024-02-20 20:21:03 +0100

    Fixed some small issues with installing k3s

--- installation/worker-node.sh
+++ installation/worker-node.sh
@@ -5,6 +5,5 @@ echo "Setting up control node configuration"
 bash $HOME/repos/k3s-pi-scripts/installation/kubelet-config-file.sh
 bash $HOME/repos/k3s-pi-scripts/installation/upgrade.sh
 bash $HOME/repos/k3s-pi-scripts/installation/k3s-worker-node-install.sh
-bash $HOME/repos/k3s-pi-scripts/installation/helm-install.sh
 
 echo "Control node configuration set up."
