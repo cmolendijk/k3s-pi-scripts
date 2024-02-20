740868f18f72d6688737222ff20903757c22db51
C. Molendijk <cmolendijk@users.noreply.github.com>
2024-02-20 21:34:36 +0100

    Update k3s-control-node-install.sh

--- installation/k3s-control-node-install.sh
+++ installation/k3s-control-node-install.sh
@@ -2,5 +2,5 @@
 
 echo "Installing k3s control node"
 TOKEN=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-curl -sfL https://get.k3s.io | K3S_TOKEN=$TOKEN sh -s --node-label 'node_type=controller'
+curl -sfL https://get.k3s.io | K3S_TOKEN=$TOKEN sh -s - server --write-kubeconfig-mode '0644' --node-taint 'node-role.kubernetes.io/master=true:NoSchedule' --kubelet-arg 'config=/etc/rancher/k3s/kubelet.config' --kube-controller-manager-arg 'terminated-pod-gc-threshold=10'
 echo "K3s control node installed with token: $TOKEN"
